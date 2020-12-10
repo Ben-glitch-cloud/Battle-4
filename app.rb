32d5d8b9d9f0377fc8c778fd9db92444eaaa1912
Benedict Lawrence <BTLawrence44@gmail.com>
2020-12-10 17:00:32 +0000

    viewing hit points

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
 require 'sinatra/base' 
 
-class Battle < Sinatra::Base  
+class Battle < Sinatra::Base    
+
+    enable :sessions 
 
     get '/' do 
-        "Testing infrastuecture working!!"
+        erb :index
+    end  
+
+    get '/play' do
+        @player_1_name = session[:player_1_name]
+        @player_2_name = session[:player_2_name] 
+        erb :play
+    end
+
+    post '/names' do 
+        session[:player_1_name] = params[:player_1_name] 
+        session[:player_2_name] = params[:player_2_name]
+        redirect '/play'
     end 
 end 
--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -1,6 +1,19 @@
-feature 'Testing infrastructure' do
-    scenario 'Can run app and check page content' do
-        visit('/') 
-        expect(page).to have_content 'Testing infrastuecture working!!'
+feature 'Enter names' do
+    scenario 'Display players' do
+        visit('/')  
+        fill_in :player_1_name, with: 'Dave' 
+        fill_in :player_2_name, with: 'Mittens' 
+        click_button 'Submit'
+        expect(page).to have_content 'Dave vs. Mittens'
+    end
+end 
+
+feature 'Hit points' do
+    scenario 'display hit points from player two' do
+        visit('/')   
+        fill_in :player_1_name, with: 'Dave' 
+        fill_in :player_2_name, with: 'Mittens' 
+        click_button 'Submit'
+        expect(page).to have_content 'Mittens 60 points' 
     end
 end
